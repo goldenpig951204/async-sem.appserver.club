7d2022f7a0ca8540911e519d26d2d69dbebc6710
Xiao Qiang <goldenpig951204@gmail.com>
2022-09-21 20:59:55 +0800

    relase mistone

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { notFound, errorHandler } = require('./middlewares/permission');
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/public', express.static(path.join(__dirname, './public')));
 
 app.use('/authorize', authorizeRouter);
 app.get('/lang', (req, res) => {
--- src/routes/semrush.js
+++ src/routes/semrush.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const { memberMiddleware } = require('../middlewares/permission');
-const semrushProxy = require('../middlewares/semrushProxy');
+const {
+    domainOverviewMiddleware,
+    keywordOverviewMiddleware,
+    semrushProxy 
+} = require('../middlewares/semrushMiddleware');
 const semrushRouter = express.Router();
 const config = require('../services/config');
 /** Set admin-related routes. */
@@ -9,14 +13,23 @@ const applyMiddleware = (req, res, next) => {
     return semrushProxy(locale)(req, res, next);
 }
 
-semrushRouter.use('/', memberMiddleware, (req, res, next) => {
-    let contentType = req.headers['content-type'];
-    if (contentType && contentType.includes('application/json')) {
-        req.headers["content-type"] = "application/json; charset=UTF-8";
-    }
-    next();
-}, express.json(), (req, res, next) => {
-    next();
-}, applyMiddleware);
+semrushRouter.use(
+    '/', 
+    memberMiddleware, 
+    (req, res, next) => {
+        let contentType = req.headers['content-type'];
+        if (contentType && contentType.includes('application/json')) {
+            req.headers["content-type"] = "application/json; charset=UTF-8";
+        }
+        next();
+    }, 
+    express.json(), 
+    (req, res, next) => {
+        next();
+    }, 
+    domainOverviewMiddleware, 
+    keywordOverviewMiddleware, 
+    applyMiddleware
+);
 
 module.exports = semrushRouter;
